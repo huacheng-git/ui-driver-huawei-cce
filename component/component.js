c16b41e8454b952ef66d8ca9ef710d29826b2053
huacheng <cheng@HuachengdeMacBook-Pro.local>
2023-03-06 09:56:24 +0800

    Update k8s version

--- component/component.js
+++ component/component.js
@@ -138,8 +138,8 @@ export default Ember.Component.extend(ClusterDriver, {
       rancherEnabled: false,
     },
     {
-      label: 'v1.19',
-      value: 'v1.19',
+      label: 'v1.25(Beta)',
+      value: 'v1.25',
       rancherEnabled: false,
     },
   ],
